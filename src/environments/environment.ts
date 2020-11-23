ded25d7cdfa0249734c181461b38c734269adb73
cristiano-cazita <31074886+cristiano-cazita@users.noreply.github.com>
2020-11-23 20:38:16 -0300

    Configurações de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
   apiUrl: "https://onedatacarlocal-api.azurewebsites.net/api",
   chaveMD5: '15181AS9819F8W1F9W81F98E1F9E8F19EW38G193W8G13G198G149THTYJ5YU5UIKL5IUL1RETREG1FEW189EFW918FEW1',
   AlertDebug: true,
-  // codigoSistema: 1, // Lava jato
+   codigoSistema: 1, // Lava jato
   // codigoSistema: 2, // Troca de óleo
   // codigoSistema: 3, // Oficina
-  codigoSistema: 4, // Estacionamento
+  // codigoSistema: 4, // Estacionamento
 };
 
 /*
