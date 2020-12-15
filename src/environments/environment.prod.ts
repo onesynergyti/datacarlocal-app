1e108b8ec4914c4937467a2d2c0db5295f4fd9a6
cristiano-cazita <31074886+cristiano-cazita@users.noreply.github.com>
2020-12-15 14:28:42 -0300

    Propriedades para contagem de propagandas não exibidas.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  bloqueioPropagandasPerdidas: 6,
+  alertaPropagandasPerdidas: 3,
   chaveMD5: '15181AS9819F8W1F9W81F98E1F9E8F19EW38G193W8G13G198G149THTYJ5YU5UIKL5IUL1RETREG1FEW189EFW918FEW1',
   apiUrl: "https://onedatacarlocal-api.azurewebsites.net/api",
   whatsappSuporte: '31984601435',
